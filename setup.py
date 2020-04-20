0f2a3ce5f57f4b4d906c3c8e723286a4f6fb907c
goobong <bongbonggg97@gmail.com>
2020-04-20 23:37:05 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="ycd",
+    version="v0.1.0",
+    description="Youtube comment downloader",
+    url="https://github.com/koomook/youtube-comment-downloader",
+    author="Goobong",
+    author_email="bongbonggg97@gmail.com",
+    license="Unlicense",
+    packages=["ycd"],
+    install_requires=[],
+    dependency_links=[],
+    include_package_data=True,
+    zip_safe=False,
+)
